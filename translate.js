945071ee0c3c6d1aaa96929edd9c3dd894fbac8d
Osman <96057877+ocekic@users.noreply.github.com>
2024-06-12 11:57:33 +0200

    Error solved

--- translate.js
+++ translate.js
@@ -1,27 +1,43 @@
-  function Translate() {
-    this.init = function(attribute, lng){
-      this.attribute = this.attribute;
-      this.lng = lng;
-    }
-    this.process = function() {
-      _self=this;
-      var xrhFile=new XMLHttpRequest();
-      xrhFile.open("GET", "lng/" +this.lng+".json", false);
-      xrhFile.onreadystatechange=function() {
-        if (xrhFile.readyState===4) {
-          if (xrhFile.status===200 || xrhFile.status ==0) {
-            var LngObject =JSON.parse(xrhFile.responseText);
-            var allDom = document.getElementsByTagName("*");
-            for (var i = 0; i<allDom.length; i++) {
-              var elem = allDom[i];
-              var key=elem.getAttribute(_self.attribute);
-              if (key !==null) {
-                elem.innerHTML=LngObject[key];
-              }
-            }
-          }
+function Translate() {
+  this.init = function (attribute, lng) {
+    this.attribute = attribute;
+    this.lng = lng;
+  };
+
+  this.process = async function () {
+    try {
+      const response = await fetch("lng/" + this.lng + ".json");
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      const LngObject = await response.json();
+      const allDom = document.getElementsByTagName("*");
+      for (let i = 0; i < allDom.length; i++) {
+        const elem = allDom[i];
+        const key = elem.getAttribute(this.attribute);
+        if (key !== null && LngObject[key]) {
+          elem.innerHTML = LngObject[key];
         }
       }
-  }
+    } catch (error) {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
+    }
+  };
+}
 
-  
+document.addEventListener("DOMContentLoaded", function () {
+  const translator = new Translate();
+  translator.init("data-translate", "fr");
+  translator.process();
+
+  document
+    .getElementById("languageSelect")
+    .addEventListener("change", function () {
+      const language = this.value;
+      translator.init("data-translate", language);
+      translator.process();
+    });
+});
